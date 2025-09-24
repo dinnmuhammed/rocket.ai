985ab27775bcca5638c4f9e0dd3cad193e590376
dinnmuhammed <96682947+dinnmuhammed@users.noreply.github.com>
2025-09-24 22:13:48 +0500

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
+<script>
+  const { createClient } = supabase;
+
+  // 🔑 Replace these with your real values from Supabase
+  const SUPABASE_URL = "https://sursennpdzqvdxfajzdt.supabase.co";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1cnNlbm5wZHpxdmR4ZmFqemR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MzI0ODYsImV4cCI6MjA3NDMwODQ4Nn0.85XSNF48z2oIf8POVpfcGNbNtFzzSF5qGjR5iMA-Hn0";
+
+  const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+</script>
